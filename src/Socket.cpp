5336b393a8dc7052fa5e287b11f1abae64e4e401
uNiversaI <uNiversal@noreply.github.com>
2015-06-03 17:07:30 +0900

    fix includes

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-#include "kodi/libXBMC_addon.h"
+#include "libXBMC_addon.h"
 #include <string>
 #include "platform/os.h"
 #include "platform/util/timeutils.h"
