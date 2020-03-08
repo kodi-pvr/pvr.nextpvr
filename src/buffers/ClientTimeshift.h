2d6528563a2f8d1151490e0b56b42c6ee28e3065
emveepee <mvallevand@gmail.com>
2020-03-08 12:31:18 -0400

    Enable seeking while paused (#112)
    Kodi reads live TV stream while paused, need to keep stream open to allow seeking and avoid crash

--- src/buffers/ClientTimeshift.h
+++ src/buffers/ClientTimeshift.h
@@ -25,6 +25,7 @@
 
 std::string UriEncode(const std::string sSrc);
 
+using namespace ADDON;
 namespace timeshift {
 
   class ClientTimeShift : public RollingFile
@@ -58,10 +59,8 @@ namespace timeshift {
     {
       if ((m_isPaused = bPause))
       {
-        // pause
+        // pause save restart position
         m_streamPosition = XBMC->GetFilePosition(m_inputHandle);
-        if (!m_complete)
-          Buffer::Close();
       }
       else
       {
