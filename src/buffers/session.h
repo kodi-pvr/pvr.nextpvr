81063a4973643146389310bf5d375e0d021c3f0e
Alwin Esch <alwin.esch@web.de>
2021-09-18 12:33:40 +0200

    update copyright year to 2021

--- src/buffers/session.h
+++ src/buffers/session.h
@@ -1,7 +1,6 @@
 /*
- *  Copyright (C) 2020 Team Kodi
+ *  Copyright (C) 2020-2021 Team Kodi (https://kodi.tv)
  *  Copyright (C) 2017 Mike Burgett
- *  https://kodi.tv
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *  See LICENSE.md for more information.
